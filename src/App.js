eaad93b5f562e4468f23eda8a38dad3fc8a98fa0
Emma <70288571+emmasarge@users.noreply.github.com>
2022-06-25 13:45:44 +0100

    add open source link

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import './styles/styles.css';
-import WeatherSearch from './components/weatherSearch';
+import "./styles/styles.css";
+import WeatherSearch from "./components/weatherSearch";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
-
-
 export default function App() {
-  return (
+  return (<>
     <div className="App container weather-app">
       <div className="weather-body mt-3">
         <WeatherSearch defaultCity={"London"} />
+        
       </div>
-    </div>
+    </div><div class="mx-auto">
+          <p class="text-center open-source">
+            This is an open source project by{" "}
+            <a href="https://github.com/emmasarge/reactjs-weather-app" target="_blank">
+              Emma Sargeant
+            </a>
+          </p>
+        </div></>
   );
 }
